41edf3aa05d1935f7a21449d23875b956df2f0f6
riago <riago@10.0.0.2>
2018-02-25 15:39:32 -0500

    ria fxml labels

--- FXPhaseOne/src/application/RootLayoutController.java
+++ FXPhaseOne/src/application/RootLayoutController.java
@@ -14,8 +14,11 @@ import java.util.List;
 import java.util.Scanner;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 
 public class RootLayoutController {
 
@@ -58,8 +61,131 @@ public class RootLayoutController {
 		System.out.println(studentList.get(0).getPassword());
 	}
 	
+	@FXML
+	private Label pollListLbl;
 	@FXML 
-	private Label dummy;
+	private Label pollNameLbl;
+	@FXML 
+	private Label poll1Lbl;
+	@FXML 
+	private Label poll2Lbl;
+	@FXML 
+	private Label poll3Lbl;
+	@FXML 
+	private Label poll4Lbl;
+	@FXML 
+	private Label poll5Lbl;
+	@FXML 
+	private Label poll6Lbl;
+	@FXML 
+	private Label questionLbl;
+	@FXML 
+	private Label option1Lbl;
+	@FXML 
+	private Label option2Lbl;
+	@FXML 
+	private Label option3Lbl;
+	@FXML 
+	private Label option4Lbl;
+	@FXML 
+	private Label option5Lbl;
+	@FXML 
+	private Label option6Lbl;
+	@FXML 
+	private Label option7Lbl;
+	@FXML 
+	private Label option8Lbl;
+	@FXML 
+	private Label option9Lbl;
+	@FXML 
+	private Label option10Lbl;
+	@FXML 
+	private Label questionLbl1;
+	@FXML 
+	private Label option1Lbl1;
+	@FXML 
+	private Label option2Lbl1;
+	@FXML 
+	private Label option3Lbl1;
+	@FXML 
+	private Label option4Lbl1;
+	@FXML 
+	private Label option5Lbl1;
+	@FXML 
+	private Label option6Lbl1;
+	@FXML 
+	private Label option7Lbl1;
+	@FXML 
+	private Label option8Lbl1;
+	@FXML 
+	private Label option9Lbl1;
+	@FXML 
+	private Label option10Lbl1;
+	@FXML 
+	private Button preview1But;
+	@FXML 
+	private Button preview2But;
+	@FXML 
+	private Button preview3But;
+	@FXML 
+	private Button preview4But;
+	@FXML 
+	private Button preview5But;
+	@FXML 
+	private Button preview6But;
+	@FXML 
+	private Button result1But;
+	@FXML 
+	private Button result2But;
+	@FXML 
+	private Button result3But;
+	@FXML 
+	private Button result4But;
+	@FXML 
+	private Button result5But;
+	@FXML 
+	private Button result6But;
+	@FXML 
+	private Button newOp;
+	@FXML 
+	private Button inc;
+	@FXML 
+	private Button dec;
+	@FXML 
+	private Button save;
+	@FXML 
+	private Button closeOpenBut;
+	@FXML 
+	private TextField op1;
+	@FXML 
+	private TextField op2;
+	@FXML 
+	private TextField op3;
+	@FXML 
+	private TextField op4;
+	@FXML 
+	private TextField op6;
+	@FXML 
+	private TextField op7;
+	@FXML 
+	private TextField op8;
+	@FXML 
+	private TextField op9;
+	@FXML 
+	private TextField op10;
+	@FXML 
+	private TextField questiontxtfld;
+	@FXML 
+	private Tab edit;
+	@FXML 
+	private Tab view; 
+	
+	 
+	
+	
+	
+	
+
 	
 	
 	@FXML

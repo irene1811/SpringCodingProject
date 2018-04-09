a05a5fcdf6117160bdc173d852fca88494baf5d1
saach <saach@172.31.99.163>
2018-04-09 16:18:38 -0400

    oof from saach

--- FXPhaseOne/src/application/Poll.java
+++ FXPhaseOne/src/application/Poll.java
@@ -5,31 +5,15 @@ import java.util.List;
 
 public class Poll {
 	private String pollName;
-	boolean Open;
-	boolean Ninth;
-	boolean Tenth;
-	boolean Eleventh;
-	boolean Twelveth;
-	//private static List<Question> questionList;
+
 	
 	public Poll() {
 		pollName = "New Poll";
-		Open = false;
-		Ninth = true;
-		Tenth = true;
-		Eleventh = true;
-		Twelveth = true;
-		//questionList = new ArrayList<Question>();
 	}
 	
-	public Poll(String pName, boolean op, boolean nin, boolean ten, boolean ele, boolean twe, List<Question> qlist) {
+	public Poll(String pName) {
 		this.pollName = pName;
-		this.Open = op;
-		this.Ninth = nin;
-		this.Tenth = ten;
-		this.Eleventh = ele;
-		this.Twelveth = twe;
-		//this.questionList = qlist;
+
 	}
 
 	public String getPollName() {
@@ -40,56 +24,5 @@ public class Poll {
 		this.pollName = pollName;
 	}
 
-	public boolean isOpen() {
-		return Open;
-	}
-
-	public boolean isNinth() {
-		return Ninth;
-	}
-
-	public void setNinth(boolean ninth) {
-		Ninth = ninth;
-	}
-
-	public boolean isTenth() {
-		return Tenth;
-	}
-
-	public void setTenth(boolean tenth) {
-		Tenth = tenth;
-	}
-
-	public boolean isEleventh() {
-		return Eleventh;
-	}
-
-	public void setEleventh(boolean eleventh) {
-		Eleventh = eleventh;
-	}
-
-	public boolean isTwelveth() {
-		return Twelveth;
-	}
-
-	public void setTwelveth(boolean twelveth) {
-		Twelveth = twelveth;
-	}
-
-	public void setOpen(boolean open) {
-		Open = open;
-	}
-
-//	public List<Question> getQuestionList() {
-//		return questionList;
-//	}
-//
-//	public void setQuestionList(List<Question> questionList) {
-//		Poll.questionList = questionList;
-//	}
-//	
-//	public void addDefQuestion() {
-//		questionList.add(new Question());
-		
-//	}
+	
 }
